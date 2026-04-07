d6652bd16584a9f2f02adeadc43725d5c07adf76
Murilo Studzinski <studzinskimurilo@gmail.com>
2026-04-07 15:18:59 -0300

    Create Main.java in src/com/crud/view

---
+++ src/com/crud/view/Main.java
@@ -0,0 +1,8 @@
+package com.crud.view;
+
+public class Main {
+    public static void main(String[] args) {
+        SplashScreen splash = new SplashScreen();
+        splash.setVisible(true);
+    }
+}
